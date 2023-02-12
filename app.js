3ec9c9b8b08e09d8e2cd3aa3c109e9aacf617f5c
antoniapisano <antoniapisano@campus.masterschool.com>
2023-02-12 15:16:28 +0000

    Part I - Setup

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use ((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-    res.json({message: "Welcome to the Unsplash API!"})
+    res.status(200).json({message: "Welcome to the Unsplash API!"})
 })
 
 app.listen(process.env.PORT, () => {
