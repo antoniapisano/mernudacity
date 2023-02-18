3a3bf1638f7c3d5148e61cca47ddc2eb8f6d9c65
antoniapisano <antoniapisano@campus.masterschool.com>
2023-02-18 17:59:50 +0000

    with client id

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,11 +1,12 @@
 const axios = require("axios");
 const gotBaseURL = "https://api.unsplash.com/photos";
 const unsplashAK = process.env.UNSPLASH_ACCESS_KEY;
+const clientId = "?client_id="
 
 const getPhotos = async (req, res, next) => {
   try {
     const response = await axios.get(
-      `${gotBaseURL}?client_id=${unsplashAK}`
+      `${gotBaseURL}${clientId}${unsplashAK}`
     );
     const photos = response.data.map((photo) => photo.urls.raw);
     res.status(200).json(photos);
