be9c399683c790619174004cc4a8878d3a6a2c1e
antoniapisano <antoniapisano@campus.masterschool.com>
2023-03-26 18:47:48 +0100

    second corrections

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,35 +2,27 @@ const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel.js')
 
+
 const protect = asyncHandler(async (req, res, next) => {
-  let token
+  let token;
 
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
+    req.headers.authorization.startsWith("Bearer")
   ) {
-    try {
-      // Get token from header
-      token = req.headers.authorization.split(' ')[1]
+    token = req.headers.authorization.split(" ")[1];
 
-      // Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const encoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      // Get user from the token
-      req.user = await User.findById(decoded.id).select('-password')
+    req.user = await User.findById(encoded.id).select("-password");
 
-      next()
-    } catch (error) {
-      console.log(error)
-      res.status(401)
-      throw new Error('Not authorized')
-    }
+    next();
   }
 
   if (!token) {
-    res.status(401)
-    throw new Error('Not authorized, no token')
+    res.status(401);
+    throw new Error("Not authorized, no token");
   }
-})
+});
 
 module.exports = { protect }
--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,15 +1,20 @@
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500
-  
-    res.status(statusCode)
-  
-    res.json({
-      message: err.message,
-      stack: process.env.NODE_ENV === 'development' ? null : err.stack,
-    })
+  const statusCode = res.statusCode ? res.statusCode : 500;
+
+  if (
+    err.name === "JsonWebTokenError" || err.name === "TokenExpiredError" || err.name === "SyntaxError"
+  ) {
+    res.status(401);
+    err.message = "Authorization Error";
   }
+
+  res.status(statusCode);
+
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
   
-  module.exports = {
-    errorHandler,
-  }
+  module.exports = { errorHandler }
   
